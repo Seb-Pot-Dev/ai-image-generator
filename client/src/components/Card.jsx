c8d4b8c614a67234a177ccb8492adf4c605440e6
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2024-09-14 15:52:23 +0200

    back-end setup + connect

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { download } from '../assets';
 import { downloadImage } from '../utils';
 
@@ -14,11 +12,11 @@ const Card = ({ _id, name, prompt, photo }) => (
       <p className="text-white text-sm overflow-y-auto prompt">{prompt}</p>
 
       <div className="mt-5 flex justify-between items-center gap-2">
-        <div className="flex items-center gap-2">
-          <div className="w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold">{name[0]}</div>
-          <p className="text-white text-sm">{name}</p>
+        <div className="flex items-center gap-2 overflow-hidden">
+          <div className="w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold flex-shrink-0">{name[0]}</div>
+          <p className="text-white text-sm truncate">{name}</p>
         </div>
-        <button type="button" onClick={() => downloadImage(_id, photo)} className="outline-none bg-transparent border-none">
+        <button type="button" onClick={() => downloadImage(_id, photo)} className="outline-none bg-transparent border-none flex-shrink-0">
           <img src={download} alt="download" className="w-6 h-6 object-contain invert" />
         </button>
       </div>
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Card, FormField, Loader } from '../components';
 
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
